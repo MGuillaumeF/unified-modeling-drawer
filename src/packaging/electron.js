46db6d4335581a85fafa6758fd06cc75c4441b85
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-07-13 21:31:20 +0200

    Ajout de jon github

--- src/packaging/electron.js
+++ src/packaging/electron.js
@@ -34,7 +34,7 @@ function createWindow() {
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : url.format({
-          pathname: path.join(__dirname, "../build/index.html"),
+          pathname: path.join(__dirname, "../../build/index.html"),
           protocol: "file:",
           slashes: true
         })
