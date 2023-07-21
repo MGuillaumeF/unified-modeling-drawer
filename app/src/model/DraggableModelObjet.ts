84fadf7787cc917134b07a92dda4b80597220c20
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-07-22 01:44:26 +0200

    Update DraggableModelObjet.ts

--- app/src/model/DraggableModelObjet.ts
+++ app/src/model/DraggableModelObjet.ts
@@ -3,9 +3,6 @@ import DataModelObject from "./DataModelObject";
 export default class DraggableModelObjet extends DataModelObject {
   private _x = 0;
   private _y = 0;
-  constructor() {
-    super();
-  }
   public get x(): number {
     return this._x;
   }
