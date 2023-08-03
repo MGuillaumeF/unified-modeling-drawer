47956744eb435e031bf750c6f38ab06bcb56f247
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-08-03 09:29:10 +0200

    Update ConfigurationManager.ts

--- packaging/src/ConfigurationManager.ts
+++ packaging/src/ConfigurationManager.ts
@@ -1,18 +1,36 @@
 import { readFileSync } from "fs";
 import path from "path";
+import { Validator } from "jsonschema";
+
+export interface Configuration {
+  language : string;
+}
+
 
 export default class ConfigurationManager {
   private _filepath : string;
-  private _configuration : any;
+  private _configuration : Configuration = { language : "fr"};
   private static _instance : ConfigurationManager | null = null;
   private constructor(filename ?: string) {
     this._filepath = filename !== undefined ? path.resolve(__dirname, filename) : path.resolve(__dirname, "configuration.json")
-    this._configuration = JSON.parse(readFileSync(this._filepath).toString());
+    const configurationFileContent = JSON.parse(readFileSync(this._filepath).toString());
+    if (isValid(configurationFileContent)) {
+      this._configuration = configurationFileContent;
+    } else {
+      throw new Error("invalid configuration file content")
+    }
   }
   public static getInstance(filename ?: string) : ConfigurationManager  {
     if (this._instance === null) {
       this._instance = new ConfigurationManager(filename)
     }
     return this._instance;
+  }
+
+  private static isValid(data : unknown) : data is Configuration {
+    const validator = new Validator();
+    const schema = {"language": "string"};
+    const res = (validator.validate(data, schema));
+    return res.valid;
   }
 }
