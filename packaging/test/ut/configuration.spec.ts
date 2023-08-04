4549e4430f67af5234c67d09dc0e2d2ec5e76ee6
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-08-04 07:11:56 +0200

    Update configuration.spec.ts

--- packaging/test/ut/configuration.spec.ts
+++ packaging/test/ut/configuration.spec.ts
@@ -1,26 +1,57 @@
 import { expect } from "chai";
 import ConfigurationManager, { ConfigurationError } from "../../src/ConfigurationManager";
+import path from "path";
+import { existsSync, writeFileSync, unlinkFileSync } from "fs";
+
+const INVALID_JSON_CONFIG = path.resolve(__dirname, ".invalid-configuration.json");
 
 describe("ConfigurationManager Test Suite", function () {
   it("Call instance with not existing file in strict mode", function () {
-    try {
-      ConfigurationManager.getInstance({filename : "notExist", strict : true});
-    } catch (e) {
-      console.error("error raised", e);
+    expect(function(){
+      try {
+        ConfigurationManager.getInstance({filename : "notExist", strict : true});
+      } catch (e) {
+        console.error("error raised", e);
 
-      expect(e instanceof ConfigurationError).to.equal(
-        true,
-        "Test error is congiguration manager error instance"
-      );
-      if (e instanceof ConfigurationError) {
-        console.error("error message", e.message);
-        console.error("error stack", e.stack);
-        console.error("error string", e.toString());
-        expect(e.message).to.equal(
-          "invalid configuration file reading",
-          "test message of custom error"
+        expect(e instanceof ConfigurationError).to.equal(
+          true,
+          "Test error is congiguration manager error instance"
         );
+        if (e instanceof ConfigurationError) {
+          console.error("error message", e.message);
+          console.error("error stack", e.stack);
+          console.error("error string", e.toString());
+        }
+        throw e;
       }
-    }
+    }).to.throw('invalid configuration file reading');
+  });
+  it("Call instance with existing bad json file in strict mode", function () {
+    before(() => {
+      writeFileSync(INVALID_JSON_CONFIG, JSON.stringify({content : "no language"}, null, 2))
+    ));
+    expect(function(){
+      try {
+        ConfigurationManager.getInstance({filename : "notExist", strict : true});
+      } catch (e) {
+        console.error("error raised", e);
+
+        expect(e instanceof ConfigurationError).to.equal(
+          true,
+          "Test error is congiguration manager error instance"
+        );
+        if (e instanceof ConfigurationError) {
+          console.error("error message", e.message);
+          console.error("error stack", e.stack);
+          console.error("error string", e.toString());
+        }
+        throw e;
+      }
+    }).to.throw('invalid configuration file content');
+    after(() => {
+      if (existsSync(INVALID_JSON_CONFIG) {
+        unlinkFileSync(INVALID_JSON_CONFIG)
+      }
+    });
   });
 });
