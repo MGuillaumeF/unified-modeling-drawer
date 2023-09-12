a4a1a180b41c89ced54e92e07f746c4d8679ed79
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-09-12 11:06:27 +0000

    [AUTO] prettier format write

--- app/src/exchanges.ts
+++ app/src/exchanges.ts
@@ -4,9 +4,12 @@ export function add(projectObject: IProjectObject): void {
   if ("electronAPI" in window) {
     window.electronAPI.createModel(projectObject);
   } else if ("parent" in window) {
-    (window as Window).parent.postMessage({
-      messageType: "add-project",
-      project: projectObject
-    }, window.origin);
+    (window as Window).parent.postMessage(
+      {
+        messageType: "add-project",
+        project: projectObject
+      },
+      window.origin
+    );
   }
 }
